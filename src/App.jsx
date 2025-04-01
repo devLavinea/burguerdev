c3db46ad5f7cc5af70827dfae1757cf855ea4906
devLavinea <lavinea2411@gmail.com>
2025-04-01 00:29:46 -0300

    correções

--- src/App.jsx
+++ src/App.jsx
@@ -343,7 +343,7 @@ const App = () => {
       )}
 
       {/* Footer */}
-      <footer className="w-full bg-red-600  py-4 fixed bottom-0 z-40 flex items-center justify-center">
+      <footer className="w-full bg-red-600  py-3.5 fixed bottom-0 z-40 flex items-center justify-center">
         <button
           className="flex cursor-pointer items-center  text-[19px] gap-2 text-white font-bold"
           id="cart-btn"
